ca69a01f44132ceac9d177912632bbeafb5ed753
OUlmachi-Yassir <oulmachi.yassir@gmail.com>
2025-02-28 11:07:36 +0000

    Add: new functionality for users service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Put, Delete, Body } from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from './model/user.model';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  async getAllUsers(): Promise<User[]> {
+    return this.userService.findAll();
+  }
+
+  @Get(':id')
+  async getUser(@Param('id') id: string): Promise<User> {
+    return this.userService.findOne(id);
+  }
+
+  @Put(':id')
+  async updateUser(@Param('id') id: string, @Body() updateData: Partial<User>): Promise<User> {
+    return this.userService.update(id, updateData);
+  }
+
+  @Delete(':id')
+  async deleteUser(@Param('id') id: string): Promise<{ message: string }> {
+    return this.userService.remove(id);
+  }
+}
