248f2edd7fca198d1a7e8e05042c46571ec2a8ae
OUlmachi-Yassir <oulmachi.yassir@gmail.com>
2025-03-21 12:25:35 +0000

    Fix somme secretkey

--- src/Middleware/auth/jwt-auth.guard.ts
+++ src/Middleware/auth/jwt-auth.guard.ts
@@ -1,5 +1,9 @@
 import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
 import * as jwt from 'jsonwebtoken';
+import * as dotenv from 'dotenv';
+
+dotenv.config(); // Load environment variables
+
 
 @Injectable()
 export class JwtAuthGuard implements CanActivate {
@@ -9,8 +13,13 @@ export class JwtAuthGuard implements CanActivate {
 
     if (!token) throw new UnauthorizedException('Accès refusé.');
 
+    const SECRET_KEY = process.env.SECRET_KEY;
+    if (!SECRET_KEY) {
+      throw new Error('SECRET_KEY is not defined. Please set it in your .env file.');
+    }
+
     try {
-      const decoded = jwt.verify(token, 'SECRET_KEY');
+      const decoded = jwt.verify(token, SECRET_KEY);
       request.user = decoded;
       return true;
     } catch {
